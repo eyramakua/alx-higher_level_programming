b69b38fd3251d39f7f841d991e696ba97af1d4c9
eyramakua <eyramagginie@gmail.com>
2023-08-29 03:10:40 -0700

    2-args.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+import sys
 
 if __name__ "__main__":
     "Handles basic operators"
-    from calculator_1 import add, sub, div, mul
-    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -16,4 +16,4 @@ if __name__ "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2](a, b))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-num = len(sys.argv) - 1
-
 if __name__ == "__main__":
-    ""
-    Print the number of and list of its arguments.
-    ""
-
-    if num == 0:
-        print("0 arguments.")
-    elif num == 1:
-        print("1 argument:")
+    import sys
+    if len(sys.argv) != 2:
+        print("{:d}  arguments:".format(len(sys.argv) - 1))
     else:
-        print("{} arguments:".format(num))
-
-    for i in range(num):
-        print("{}:{}".format(i + 1, sys.argv[i + 1]))
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}:{:s}".format(i, sys.argv[i]))
