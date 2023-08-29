fa3a73e095350701618b402b0d8572083144374c
eyramakua <eyramagginie@gmail.com>
2023-08-29 04:28:35 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
     count = 0
     for i in range(x):
         try:
@@ -10,4 +9,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             continue
         print("")
-        return count
+        return (count)
