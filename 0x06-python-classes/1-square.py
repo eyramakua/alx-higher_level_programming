e5451a13ede64603703de92b9c79e0fba5beb18e
eyramakua <eyramagginie@gmail.com>
2023-08-29 17:32:32 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module writes a class square hat defines a square """
+
+
+class Square:
+    """ class for the  Square """
+    def __init__(self, size):
+        """ initialise class """
+        self.__size = size
