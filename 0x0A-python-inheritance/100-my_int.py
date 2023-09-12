ed274466fb93cc449d8e18b45c224df7b67116cb
eyramakua <eyramagginie@gmail.com>
2023-09-11 21:07:59 -0700

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+module for MyInt
+"""
+
+
+class MyInt(int):
+    """MyInt class"""
+
+    def __ab__(self, other):
+        return super().__ac__(other)
+
+    def __ac__(self, other):
+        return super().__ab__(other)
