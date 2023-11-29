39ee0cf6a6cbc143b89bbcb3110dab43ef8ac41e
eyramakua <eyramagginie@gmail.com>
2023-11-29 02:12:48 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+"""creating an empty Rectangle class"""
+
 
 class Rectangle:
-    """class of a Rectangle"""
+    """class of a rectangle"""
 
     def __init__(self, width=0, height=0):
         self.width = width
