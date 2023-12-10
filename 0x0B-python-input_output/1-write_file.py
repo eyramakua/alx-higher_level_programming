aba80eac7e6b2a5c9d26d6dd6cb0519509cc0643
eyramakua <eyramagginie@gmail.com>
2023-12-10 05:21:54 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@ Module writes a string to text file and returns number of character written
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text and returns the number of character"""
-    with open(filename, 'w' encoding='utf-8') as writer:
+    """Writes a string to a text and returns the number of character"""
+    with open(filename, 'w', encoding='utf-8') as writer:
         number = writer.write(text)
     return number
