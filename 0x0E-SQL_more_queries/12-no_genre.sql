138111a944778c6783c0a63918a16e73539fd655
eyramakua <eyramagginie@gmail.com>
2024-01-17 21:32:24 -0800

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres_genre.id;
+ORDER BY tv_shows.title ASC, tv_show_genres_genre.id ASC;
