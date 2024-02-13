aea28bdbdfa93ac20e3a33ad63d8f8380c644efb
eyramakua <eyramagginie@gmail.com>
2024-02-13 08:21:29 -0800

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (weight, height) {
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-      return;
-    }
-    this.width = weight;
-    this.height = height;
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
 
-module.exports = Rectangle;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let a = 'X'.repeat(this.width) + '\n';
+    a = a.repeat(this.height);
+    console.log(a.slice(0, -1));
+  }
+};
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    [this.height, this.width] = [this.height * 2, this.width * 2];
+  }
+};
