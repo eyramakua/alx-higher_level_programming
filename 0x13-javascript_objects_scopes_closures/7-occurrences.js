adc98f90b5054feeeda4c1da4143a811f1040c0e
eyramakua <eyramagginie@gmail.com>
2024-02-13 07:27:34 -0800

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nb0ccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   const count = list.reduce((n, val) => n + (val === searchElement), 0);
   return count;
 };
