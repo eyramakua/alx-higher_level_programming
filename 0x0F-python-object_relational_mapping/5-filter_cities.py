993d094f56991ea24f27801856f1e3ec378ccd37
eyramakua <eyramagginie@gmail.com>
2024-02-18 10:00:46 -0800

    Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    curs = db.cursor()
+    curs.execute("""SELECT name FROM cities WHERE (SELECT id FROM states WHERE
+                  BINARY name = %s) = state_id""", (sys.argv[4],))
+    states = curs.fetchall()
+    print(", ".join([state[0] for state in states]))
+    curs.close()
+    db.close()
