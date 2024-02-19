8b6f7c5213e2ce5e9b3fd103b2400c038bb9073d
eyramakua <eyramagginie@gmail.com>
2024-02-19 02:47:20 -0800

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,4 +12,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement="auto")
-    name = Column(String(128)0, nullable=False)
+    name = Column(String(128), nullable=False)
