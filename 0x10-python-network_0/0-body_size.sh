3a9ff3e8961a8ce8181923247dd37e53fc2a1358
eyramakua <eyramagginie@gmail.com>
2024-02-29 08:18:25 -0800

    correction of task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that displays the size of the body of the response
-curl -sI "$1" | grep -i Content-Length | cut -d '' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
