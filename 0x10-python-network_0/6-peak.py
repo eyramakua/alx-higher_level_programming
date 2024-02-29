08a4ced681794016de1bd625636ec74a150ee5bd
eyramakua <eyramagginie@gmail.com>
2024-02-29 09:04:36 -0800

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+'''
+module to find peak
+'''
+
+
+def find_peak(listint):
+    '''Function that returns peak value in a list'''
+    if listint:
+        n = listint[0]
+        for j in listint:
+            if j > n:
+                n = j
+        return n
+    else:
+        return None
