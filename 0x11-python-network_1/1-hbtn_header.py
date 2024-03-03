a5902f9eacfb5b6317ffbcb1e8578ce4cd21f4a6
eyramakua <eyramagginie@gmail.com>
2024-03-03 10:16:44 -0800

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+pythin scipt that displays the value of the X-Request-Id variable
+'''
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.info()['X-Request-Id'])
