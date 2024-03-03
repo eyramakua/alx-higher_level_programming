d9b261894f322bfc793541b70b1decdb85e327a3
eyramakua <eyramagginie@gmail.com>
2024-03-03 14:06:33 -0800

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    json = {'q': ""}
+    if len(argv) > 1:
+        json['q'] = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", json)
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        if reponse.json():
+            print("[{}] {}".format(response.json().get('id'),
+                  response.json().get('name')))
+        else:
+            print("No result")
