a7e48c6efef8bec1eb7574ab33a67ac9bcce9bd6
eyramakua <eyramagginie@gmail.com>
2024-03-03 13:56:46 -0800

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+sends a request to the URL and displays the body of the response
+'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    statusCode = response.status_code
+    if statusCode >= 400:
+        print("Error code: " + str(statusCode))
+    else:
+        print(response.text)
