9fba8a352d2bb92b0e5d10a3041de46ef6708179
eyramakua <eyramagginie@gmail.com>
2024-03-26 09:22:27 -0700

    correction of task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, data) {
   if (error) {
-    console.log(Error);
+    console.log(error);
   } else {
     console.log(data);
   }
