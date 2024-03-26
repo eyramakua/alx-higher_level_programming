284d1c38bd5897e8a032d41ccce54a8d1f796a6f
eyramakua <eyramagginie@gmail.com>
2024-03-26 09:36:08 -0700

    task 2 correction

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
-  }
+  } else {
     console.log('code:', response.statusCode);
   }
 });
